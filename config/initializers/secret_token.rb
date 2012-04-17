6c9634ff3f30df243db353a97673e8b5723d90dc
Dawn Friedland <dawnfr@uw.edu>
2012-04-16 21:21:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blipub::Application.config.secret_token = '9365b83301e826bc3ec467584774a457ced13abe914fa2fe1d000e35fb0c5f039877b38d8d7f9eadbc6406679d7e7fd7d79081f9bae041cfcae9e9fac8edea47'
